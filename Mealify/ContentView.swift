1b960c3f8fee98f5fdc87578ac9bb4d7c5802406
Joseph To <jto2@uoregon.edu>
2023-10-09 19:42:50 -0700

    Update ContentView.swift
    Basic UI for API testing

--- Mealify/ContentView.swift
+++ Mealify/ContentView.swift
@@ -1,24 +1,34 @@
-//
-//  ContentView.swift
-//  Mealify
-//
-//  Created by Joseph To on 10/9/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
+    @State private var searchResults = ["Recipe 1", "Recipe 2", "Recipe 3", "Recipe 4", "Recipe 5"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                List(searchResults, id: \.self) { result in
+                    Text(result) // Replace Text with your actual result view
+                }
+            }
+            .navigationTitle("Recipe Search")
+            .searchable(text: $searchText, placement: .navigationBarDrawer, prompt: "Search for recipes") {
+                Text("Search for recipes")
+            }
         }
-        .padding()
+    }
+    
+    private func search() {
+        // Perform your API search here and update searchResults with the results
+        // Example:
+        // YourAPIClient.search(query: searchText) { results in
+        //     searchResults = results
+        // }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
