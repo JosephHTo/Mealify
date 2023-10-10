1ec2545c5a7fcf0f172e08ec322b421dc4dbb8e8
Joseph To <jto2@uoregon.edu>
2023-10-09 19:40:20 -0700

    Initial Commit

---
+++ Mealify/MealifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealifyApp.swift
+//  Mealify
+//
+//  Created by Joseph To on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MealifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
